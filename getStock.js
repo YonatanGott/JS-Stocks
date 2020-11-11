f9165d457c3c87a5eeae6e85b3f86a0749631fa3
YonatanGott <godiner@gmail.com>
2020-11-11 11:36:27 +0200

    Milestone 3 update function

--- getStock.js
+++ getStock.js
@@ -19,17 +19,23 @@ async function getStock() {
     let stockHead = "&limit=10&exchange=NASDAQ";
     let query = document.getElementById("search").value;
     let stockSearch = stocksBase + query + stockHead;
+    let arraySymbol = [];
     let res = await fetch(stockSearch);
     let data = await res.json();
     for (let i = 0; i < data.length; i++) {
         let stockRes = data[i];
-        let name = stockRes.name;
         let symbol = stockRes.symbol;
-        let profile = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/" + symbol;
-        let res2 = await fetch(profile);
-        let data2 = await res2.json();
-        let price = data2.profile.price;
-        let changes = data2.profile.changesPercentage;
+        arraySymbol.push(symbol);
+    }
+    let symbolBatch = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/quote/"+arraySymbol.join();
+    let resSymbol = await fetch(symbolBatch);
+    let dataSymbol = await resSymbol.json();
+    for ( let j = 0; j < dataSymbol.length; j++){
+    console.log(dataSymbol[j].name);
+        let name = dataSymbol[j].name;
+        let symbol = dataSymbol[j].symbol;
+        let price = dataSymbol[j].price;
+        let changes= dataSymbol[j].changesPercentage;
         let image = "https://financialmodelingprep.com/image-stock/" + symbol + ".jpg";
         let imgItem = document.createElement("img");
         imgItem.setAttribute("src", image);
@@ -42,22 +48,27 @@ async function getStock() {
         let nameEle = document.createElement("p");
         nameEle.classList.add("nameEle");
         nameEle.textContent = name;
+        let symbolEle = document.createElement("p");
+        symbolEle.classList.add("symbolEle");
+        symbolEle.textContent = symbol;
         let priceEle = document.createElement("p");
         priceEle.classList.add("priceEle");
         priceEle.textContent = "$"+price;
         let changeEle = document.createElement("p");
         changeEle.classList.add("changeEle");
-        if (changes.indexOf("-") == -1) {
+        if (changes > 0 ) {
             changeEle.classList.add("green");
         }
-        changeEle.textContent = changes;
+        changeEle.textContent ="("+changes+")";
         link.appendChild(nameEle);
+        link.appendChild(symbolEle);
         link.appendChild(priceEle);
         link.appendChild(changeEle);
         listItem.appendChild(link);
         listItem.appendChild(imgItem);
         document.getElementById("results").appendChild(listItem);
         document.getElementById("results").appendChild(under);
+    
     }
 }
 
